f074782f05b6b1b0a3c9b65649462f822d09e236
Willians Martins <professorwillians@gmail.com>
2022-04-05 10:05:10 -0300

    add maos a obra cap 16

---
+++ capitulo-16-io/src/LendoArquivo.java
@@ -0,0 +1,21 @@
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class LendoArquivo {
+	public static void main(String[] args) {
+		try {
+			FileInputStream arquivo = new FileInputStream("arquivoX.txt");
+			DataInputStream dados = new DataInputStream(arquivo);
+			
+			while(true) {
+				char c = dados.readChar();
+				System.out.println(c);
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ capitulo-16-io/src/maosAObra/Cap16_Lab1.java
@@ -0,0 +1,37 @@
+package maosAObra;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Cap16_Lab1 {
+	public static void main(String[] args) {
+		escrever("Capítulo 16\nLaboratório 1");
+		ler();
+	}
+
+	private static void escrever(String texto) {
+		try {
+			FileOutputStream arquivo = new FileOutputStream("Cap16_Lab1.txt");
+			DataOutputStream dados = new DataOutputStream(arquivo);
+			dados.writeChars(texto);
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+
+	private static void ler() {
+		try {
+			FileInputStream arquivo = new FileInputStream("Cap16_Lab1.txt");
+			DataInputStream dados = new DataInputStream(arquivo);
+			while (dados.available() > 0) {
+				char c = dados.readChar();
+				System.out.print(c);
+			}
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+}
